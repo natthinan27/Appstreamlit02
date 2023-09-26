1d8c767e19351faeeaca34cab7981f8d7a27f61d
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-09-26 15:17:43 +0700

    .commit

--- Mystreamlit.py
+++ Mystreamlit.py
@@ -6,7 +6,5 @@ with col1 :
 with col2 :
     st.subheader('สาขาวิทยาการข้อมูล')
     st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
-st.image('./pic/02.jpg')
-st.image('./pic/03.jpg')
-st.image('./pic/04.jpg')
+
 st.header('Natthinan Khemthong')
