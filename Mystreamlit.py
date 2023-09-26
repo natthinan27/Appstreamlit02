b54a24b89f6f5281da46fca8023a432f814f5205
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-09-26 15:24:17 +0700

    .commit

--- Mystreamlit.py
+++ Mystreamlit.py
@@ -6,5 +6,10 @@ with col1 :
 with col2 :
     st.subheader('สาขาวิทยาการข้อมูล')
     st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
-
-st.header('Natthinan Khemthong')
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
