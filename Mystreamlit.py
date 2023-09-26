6959e9e89f1d84f525cdfc1529d9960fdc35598d
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-09-26 15:13:56 +0700

    ใแนททระ

--- Mystreamlit.py
+++ Mystreamlit.py
@@ -1,5 +1,11 @@
 import streamlit as st
 st.image('./pic/01.jpg')
+col1,col2=st.colums(2)
+col1 with:
+    st.header('ณัฐินันท์ เข็มทอง')
+col2 with:
+    st.subheader('สาขาวิทยาการข้อมูล')
+    st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
 st.image('./pic/02.jpg')
 st.image('./pic/03.jpg')
 st.image('./pic/04.jpg')
